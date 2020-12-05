3b2e1eee8f09b191e35922d681034cd47a83c040
Lennart <lennartnat@live.com>
2020-12-05 16:14:53 +0900

    Update db.url after mlab > atlas migration

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     port: process.env.PORT || 5000
   },
   db: {
-    url: 'mongodb://lennart:lenn4rt@ds055945.mlab.com:55945/kiji'
+    url: 'mongodb+srv://lennart:lenn4rt@kiji.5fego.mongodb.net/kiji?retryWrites=true&w=majority'
   }
 };
